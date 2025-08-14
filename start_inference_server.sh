57d6e63aedb2180383203638950760659e2e461f
Tobias Luebbe <tobias.luebbe@tngtech.com>
2025-08-14 20:45:53 +0200

    files for scheduling and inferencing finetunes

---
+++ start_inference_server.sh
@@ -0,0 +1,5 @@
+python scripts/inference_service.py \
+    --model-path /home/innovation-hacking/luebbet/models/eval_08_14/diff_small_obs_absolute_actions/checkpoint-20000/\
+    --embodiment_tag new_embodiment \
+    --data_config "tng_ur5_schunk_2_cams_sim" \
+    --server
