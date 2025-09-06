de6e4d1b5b5d4a8c779ddd8da845fa16571debab
Tobias Luebbe <tobias.luebbe@tngtech.com>
2025-09-06 14:17:00 +0200

    update shell files

--- schedule_finetunes.sh
+++ schedule_finetunes.sh
@@ -1,9 +1,28 @@
 python scripts/gr00t_finetune.py \
     --train-dataset-path /home/innovation-hacking/luebbet/dev/important_datasets/experiments/01_kinematics/cal3_rand150_dim0-2_moving_target/ \
-    --validation-dataset-path /home/innovation-hacking/luebbet/dev/important_datasets/experiments/01_kinematics/cal3_rand150_dim0-2_moving_target/ \
-    --eval_steps 50 \
-    --output-dir /home/innovation-hacking/luebbet/models/eval_09_05/AbsJoint-State_DeltaJoint-Action \
-    --max-steps 111 \
+    --validation-dataset-path /home/innovation-hacking/luebbet/dev/important_datasets/experiments/validation/ \
+    --eval_steps 0 \
+    --output-dir /home/innovation-hacking/luebbet/models/eval_09_16/AbsJoint-AbsTCP-State_DeltaJoint-Action \
+    --max-steps 35000 \
+    --data-config "tng_ur5_AbsJointAndAbsTCPState_DeltaJointAction_2Cams" \
+    --embodiment-tag new_embodiment \
+    --video-backend torchvision_av \
+    --batch-size 16 \
+    --no-tune-llm \
+    --no-tune-visual \
+    --tune-projector \
+    --tune-diffusion-model \
+    --report-to tensorboard \
+    --save-steps 1000 \
+    --train-modality-tokenizer \
+
+
+python scripts/gr00t_finetune.py \
+    --train-dataset-path /home/innovation-hacking/luebbet/dev/important_datasets/experiments/01_kinematics/cal3_rand150_dim0-2_moving_target/ \
+    --validation-dataset-path /home/innovation-hacking/luebbet/dev/important_datasets/experiments/validation/ \
+    --eval_steps 0 \
+    --output-dir /home/innovation-hacking/luebbet/models/eval_09_16/AbsJoint-State_DeltaJoint-Action \
+    --max-steps 35000 \
     --data-config "tng_ur5_AbsJointState_DeltaJointAction_2Cams" \
     --embodiment-tag new_embodiment \
     --video-backend torchvision_av \
@@ -13,3 +32,25 @@ python scripts/gr00t_finetune.py \
     --tune-projector \
     --tune-diffusion-model \
     --report-to tensorboard \
+    --save-steps 1000 \
+    --train-modality-tokenizer \
+
+
+python scripts/gr00t_finetune.py \
+    --train-dataset-path /home/innovation-hacking/luebbet/dev/important_datasets/experiments/01_kinematics/cal3_rand150_dim0-2_moving_target/ \
+    --validation-dataset-path /home/innovation-hacking/luebbet/dev/important_datasets/experiments/validation/ \
+    --eval_steps 1000 \
+    --output-dir /home/innovation-hacking/luebbet/models/eval_09_16/AbsJoint-AbsTCP-State_DeltaJoint-Action_Validation \
+    --max-steps 35000 \
+    --data-config "tng_ur5_AbsJointAndAbsTCPState_DeltaJointAction_2Cams" \
+    --embodiment-tag new_embodiment \
+    --video-backend torchvision_av \
+    --batch-size 16 \
+    --no-tune-llm \
+    --no-tune-visual \
+    --tune-projector \
+    --tune-diffusion-model \
+    --report-to tensorboard \
+    --save-steps 1000 \
+    --train-modality-tokenizer \
+    --eval-batch-size 8 \
--- start_model_eval.sh
+++ start_model_eval.sh
@@ -1,10 +1,10 @@
 python scripts/eval_policy.py \
     --plot \
-    --dataset_path /home/innovation-hacking/luebbet/dev/important_datasets/pick_and_place_increased_obs_limited_rotation_corrupted \
-    --model_path /home/innovation-hacking/luebbet/models/eval_08_14/diff_small_obs_delta_actions/checkpoint-13000/ \
+    --dataset_path /home/innovation-hacking/luebbet/dev/important_datasets/experiments/01_kinematics/cal3_rand150_dim0-2_moving_target/ \
+    --model_path /home/innovation-hacking/luebbet/models/eval_09_16/AbsJoint-State_DeltaJoint-Action/checkpoint-35000 \
     --embodiment_tag new_embodiment \
-    --data_config "tng_ur5_schunk_2_cams_sim_delta_actions" \
+    --data_config "tng_ur5_AbsJointState_DeltaJointAction_2Cams" \
     --video_backend torchvision_av \
     --modality_keys delta_robot_arm delta_gripper \
-    --save_plot_path /home/innovation-hacking/luebbet/models/eval_08_14/diff_small_obs_delta_actions/checkpoint-13000/plots_unseen \
-    --trajs 10
+    --save_plot_path /home/innovation-hacking/luebbet/models/eval_09_16/AbsJoint-State_DeltaJoint-Action/checkpoint-35000/plots \
+    --trajs 1
